1795c934dad8f7b5493f64225329c411dbb989ac
Usama Ali <usamashiekh33@gmail.com>
2022-04-09 00:44:19 +0500

    implment listview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,30 @@
+import 'package:first_project/models/catelog.dart';
 import 'package:first_project/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => catelogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Catalog App",
         ),
       ),
-      body: const Center(
-        child: Text("Usama Ali Ki sadsa sads asd"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
       ),
       drawer: const MyDrawer(),
     );
---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,30 @@
+import 'package:first_project/models/catelog.dart';
+import 'package:flutter/material.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+  const ItemWidget({Key? key, required this.item})
+      : assert(item != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0.0,
+      child: ListTile(
+        onTap: () {
+          print("${item.name} ");
+        },
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          "\$${item.price}",
+          textScaleFactor: 1.2,
+          style: const TextStyle(
+              color: Colors.deepPurple, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
